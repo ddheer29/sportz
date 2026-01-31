e4499630d606ab04f32e4606c6d6d29b5acef697
Divyang Dheer <divyangdheer11@gmail.com>
2026-02-01 00:44:22 +0530

    implement matches post and get routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchesRouter } from './routes/matches.js';
 const app = express();
 const PORT = 8000;
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json('Welcome to the Express.js server!');
 });
+
+app.use("/matches", matchesRouter);
 
 // Start server and log URL
 app.listen(PORT, () => {
